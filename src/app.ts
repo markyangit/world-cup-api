fdd27d6633c7d3938c15ba9091a92f43987db19b
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2022-03-04 09:50:04 -0300

    fix linters

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import connection from './model/connection';
+import router from './routes';
+
+class App {
+  public express: express.Express;
+
+  public connection: void;
+
+  constructor() {
+    this.express = express();
+    this.middlewares();
+    this.router();
+    this.connection = connection('mongodb://localhost:27017/world_cups');
+  }
+
+  private middlewares(): void {
+    this.express.use(express.json());
+  }
+
+  private router() {
+    this.express.use(router);
+  }
+}
+
+export default App;
